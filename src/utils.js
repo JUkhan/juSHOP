4134259394f7aab2c57502ed7cf509ca3d4e9fef
jukhan <jukhan@Kamruls-MacBook-Pro.local>
2019-05-02 19:59:59 +0600

    clean code

--- src/utils.js
+++ src/utils.js
@@ -22,21 +22,3 @@ export function persistState() {
     });
 }
 
-export function useSubscriptionsx(states) {
-    const [state, dispatch] = useReducer((state, action) => {
-        return action.type ? { ...state, [action.type]: action.payload } : state
-    }, states.reduce((prev, next) => { prev[next] = storeCtx().store.states[next] ? storeCtx().store.states[next].initialState : {}; return prev; }, {}));
-    useEffect(() => cleanupSubscriptions(dispatch, states), [])
-    return state
-}
-function cleanupSubscriptions(dispatch, states) {
-    var subs = new Subscription();
-    states.forEach(stateName => {
-        subs.add(storeCtx().select(stateName).subscribe(data => {
-            dispatch({ type: stateName, payload: data })
-        }));
-    });
-    return function () {
-        subs.unsubscribe();
-    };
-}
